6f71351d4948c16eca94106adece9d3a9a72edab
minkinew <44183457+minkinew@users.noreply.github.com>
2019-10-12 15:45:59 +0900

    JPEG Compression

---
+++ hw09/my_decoding.m
@@ -0,0 +1,136 @@
+﻿function img = my_decoding(zigzag,row,col)
+% Compress Image using portion of JPEG
+% zigzag : result of zigzag scanning
+% img    : GrayScale Image
+
+% Zigzag scanning
+N = 8;
+count = 1;
+pad_img = zeros(row,col);
+for i=1:N:row
+    for j=1:N:col
+        matrix = zigzag{count};
+        array = zeros(N, N);
+        
+        row_ = 1; % coordinate of x
+        col_ = 1; % coordinate of y
+        index = 1; % index number
+        
+        while row_ <= N && col_ <= N % move right at row_ 1
+            if (row_ == 1) && (mod(row_+col_, 2) == 0) && (col_ ~= N)
+                if matrix(index) == 999
+                    break;
+                end
+                array(row_, col_) = matrix(index);
+                col_ = col_ + 1;
+                index = index + 1;
+                
+            elseif (row_ == N) && (mod(row_+col_, 2) ~= 0) && (col_ ~= N) % move right at row_ N
+                if matrix(index) == 999
+                    break;
+                end
+                array(row_, col_) = matrix(index);
+                col_ = col_ + 1;
+                index = index + 1;
+                
+            elseif (col_ == 1) && (mod(row_+col_, 2) ~= 0) && (row_ ~= N)  % move down at col_ 1
+                if matrix(index) == 999
+                    break;
+                end
+                array(row_, col_) = matrix(index);
+                row_ = row_ + 1;
+                index = index + 1;
+                
+            elseif (col_ == N) && (mod(row_+col_, 2) == 0) && (row_ ~= N) % move down at col_ N
+                if matrix(index) == 999
+                    break;
+                end
+                array(row_, col_) = matrix(index);
+                row_ = row_ + 1;
+                index = index + 1;
+                
+            elseif (row_ ~= N) && (col_ ~= 1) && (mod(row_+col_, 2) ~= 0) % move diagonally left down
+                if matrix(index) == 999
+                    break;
+                end
+                array(row_ ,col_) = matrix(index);
+                row_ = row_ + 1;
+                col_ = col_ - 1;
+                index = index + 1;
+                
+            elseif (row_ ~= 1) && (col_ ~= N) && (mod(row_+col_, 2)==0)  % move diagonally right up
+                if matrix(index) == 999
+                    break;
+                end
+                array(row_, col_) = matrix(index);
+                row_ = row_ - 1;
+                col_ = col_ + 1;
+                index = index + 1;
+                
+            elseif (row_ == N) && (col_ == N)
+                break
+            end
+        end
+        pad_img(i:i+N-1, j:j+N-1) = array(1:N, 1:N);
+        count = count + 1;
+    end
+end
+
+% Multiply Quantization using Luminance
+Luminance =     [16 11 10 16 24 40 51 61;
+    12 12 14 19 26 58 60 55;
+    14 13 16 24 40 57 69 56;
+    14 17 22 29 51 87 80 62;
+    18 22 37 56 68 109 103 77;
+    24 35 55 64 81 104 113 92;
+    49 64 78 87 103 121 120 101;
+    72 92 95 98 112 100 103 99];
+
+for i = 1:N:row
+    for j = 1:N:col
+        pad_img(i:i+N-1, j:j+N-1) = (pad_img(i:i+N-1, j:j+N-1) .* Luminance(1:N, 1:N)) ;
+    end
+end
+
+% IDCT
+[x,y] = size(pad_img);
+IDCT = zeros(N);
+
+for i = 1:N:row
+    for j = 1:N:col
+        block = pad_img(i:i+N-1, j:j+N-1); 
+        for u = 0:N-1
+            for v = 0:N-1
+                IDCT_result = 0;
+                for q = 0:N-1
+                    for w = 0:N-1
+                        % calculate C(u), C(v)
+                        if q == 0
+                            Cu = sqrt(1/N);
+                        else
+                            Cu = sqrt(2/N);
+                        end
+                        
+                        if w == 0
+                            Cv = sqrt(1/N);
+                        else
+                            Cv = sqrt(2/N);
+                        end
+                        
+                        cos_result = cos((((2*u)+1)*q*pi)/(2*N)) * cos((((2*v)+1)*w*pi)/(2*N));
+                        Func_result = Cu * Cv * block(q+1,w+1) * cos_result;
+                        IDCT_result = IDCT_result + Func_result;
+                    end
+                end
+                IDCT(u+1, v+1) = IDCT_result;
+            end
+        end
+        pad_img(i:i+N-1, j:j+N-1) = IDCT(1:N, 1:N);
+    end
+end
+
+% Add 128
+pad_img = pad_img + 128;
+img = uint8(pad_img);
+imshow(img);
+end
---
+++ hw09/my_encoding.m
@@ -0,0 +1,175 @@
+﻿function [zigzag, wx, hy] = my_encoding(img)
+% Compress Image using portion of JPEG
+% img    : GrayScale Image
+% zigzag : result of zigzag scanning
+
+[x,y] = size(img);
+sub_img = zeros(x,y);
+img = double(img);
+
+% Subtract 128
+sub_img = img - 128;
+
+% padding
+w = 0;
+h = 0;
+
+if mod(x,8) ~= 0
+    w = 8 - mod(x,8);
+end
+if mod(y,8) ~= 0
+    h = 8 - mod(y,8);
+end
+
+pad_img = zeros(x+w, y+h);
+pad_img(1:x, 1:y) = sub_img(1:x, 1:y);
+pad_img(x+1:x+w, 1:y) = sub_img(x:-1:x-w+1, 1:y);
+pad_img(1:x, y+1:y+h) = sub_img(1:x, y:-1:y-h+1);
+pad_img(x:x+w, y:y+h) = sub_img(x:-1:x-w, y:-1:y-h);
+
+
+% DCT
+N=8;
+DCT = zeros(N);
+for i = 1:N:x
+    for j = 1:N:y
+        block = pad_img(i:i+N-1, j:j+N-1);
+        for u = 0:N-1
+            for v = 0:N-1
+                % calculate C(u), C(v)
+                if u == 0
+                    Cu = sqrt(1/N);
+                else
+                    Cu = sqrt(2/N);
+                end
+                
+                if v == 0
+                    Cv = sqrt(1/N);
+                else
+                    Cv = sqrt(2/N);
+                end
+                
+                DCT_result = 0;
+                for a = 0:N-1
+                    for b = 0:N-1
+                        cos_result = cos((((2*a)+1)*u*pi)/(2*N)) * cos((((2*b)+1)*v*pi)/(2*N));
+                        func_result = block(a+1, b+1) * cos_result  ;
+                        DCT_result = DCT_result + func_result;
+                    end
+                end
+                DCT(u+1,v+1) = Cu * Cv * DCT_result;
+            end
+        end
+        pad_img(i:i+N-1, j:j+N-1) = DCT(1:N,1:N);
+    end
+end
+
+% Divide Quantization using Luminance
+Luminance=     [16 11 10 16 24 40 51 61;
+    12 12 14 19 26 58 60 55;
+    14 13 16 24 40 57 69 56;
+    14 17 22 29 51 87 80 62;
+    18 22 37 56 68 109 103 77;
+    24 35 55 64 81 104 113 92;
+    49 64 78 87 103 121 120 101;
+    72 92 95 98 112 100 103 99];
+
+for i = 1:N:x
+    for j = 1:N:y
+        pad_img(i:i+N-1, j:j+N-1) = (pad_img(i:i+N-1, j:j+N-1) ./ Luminance(1:N, 1:N));
+    end
+end
+
+% Zigzag scanning
+scan = {}
+for i = 1:N:x
+    for j = 1:N:y
+        matrix = pad_img(i:i+N-1,j:j+N-1);
+        array = zeros(1, N*N);
+        
+        row = 1; % coordinate of x
+        col = 1; % coordinate of y
+        index = 1; % index number
+        count = 0; % count zero number
+        result = 0; 
+        
+        
+        
+        while row <= N && col <= N % move right at row 1
+            if (row == 1) && (mod(row+col, 2) == 0) && (col ~= N)
+                array(index) = matrix(row, col);
+                if matrix(row, col) == 0
+                    count = count + 1;
+                else
+                    count = 0;
+                end
+                col = col + 1;                     
+                index = index + 1;
+                
+            elseif (row == N) && (mod(row+col, 2) ~= 0) && (col ~= N) % move right at row N
+                array(index) = matrix(row, col);
+                if matrix(row, col) == 0
+                    count = count + 1;
+                else
+                    count = 0;
+                end
+                col = col + 1;                     
+                index = index + 1;
+                
+            elseif (col == 1) && (mod(row+col, 2) ~= 0) && (row ~= N) % move down at col 1
+                array(index) = matrix(row, col);
+                if matrix(row, col) == 0
+                    count = count + 1;
+                else
+                    count = 0;
+                end
+                row = row + 1;                     
+                index = index + 1;
+                
+            elseif (col == N) && (mod(row+col, 2) == 0) && (row ~= N) % move down at col N
+                array(index) = matrix(row, col);
+                if matrix(row, col) == 0
+                    count = count + 1;
+                else
+                    count = 0;
+                end
+                row = row + 1;                    
+                index = index + 1;
+                
+            elseif (row ~= N) && (col ~= 1) && (mod(row + col, 2) ~= 0) % move diagonally left down
+                array(index) = matrix(row, col);
+                if matrix(row, col) == 0
+                    count = count + 1;
+                else
+                    count = 0;
+                end
+                row = row + 1;
+                col = col - 1;   
+                index = index + 1;
+                
+            elseif (row ~= 1) && (col ~= N) && (mod(row + col, 2) == 0) % move diagonally right up
+                array(index) = matrix(row, col);
+                if matrix(row, col) == 0
+                    count = count + 1;
+                else
+                    count = 0;
+                end
+                row = row - 1;
+                col = col + 1;   
+                index = index + 1;
+                
+            elseif (row == N) && (col == N)   
+                break                              
+            end
+        end
+        
+        result = (N*N) - count;
+        array(result) = 999; % EOB
+        array
+        scan{end + 1} = array(1:result);
+    end
+end
+zigzag = scan;
+wx = x
+hy = y
+end 
